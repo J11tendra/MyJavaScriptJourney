68031681a77a566df14eb0aef8fae040d2bd9a4d
J11tendra <info.jiitendra@gmail.com>
2023-10-17 12:50:25 +0530

    added palindrome checker

---
+++ 11_PalindromeChecker/script.js
@@ -0,0 +1,7 @@
+function palindromes(string) {
+  
+  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return processedString.split('').reverse().join('') == processedString;
+}
+
+console.log(palindromes("acarissiraca"));
